6fcae9c3d5ff2a34fa455e09752db65d07af05bc
iantsai <iantsai86@gmail.com>
2015-05-05 21:15:49 -0700

    Initial Commit

---
+++ math/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  math
+//
+//  Created by Ian Tsai on 2015-05-05.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
